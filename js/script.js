50a6d71cea316303736352a37e8ef354542c903c
innarfaiza <innarfaiza@gmail.com>
2023-12-20 16:33:17 +0800

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,68 @@
-//toogle class active
-const navbarNav = document.querySelector(".navbar-nav");
-const hamburger = document.querySelector("#hamburger-menu");
-hamburger.addEventListener("click", function (e) {
-  navbarNav.classList.toggle("active");
+// search start
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+const sb = document.querySelector("#search-btn");
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+// search end
+
+
+// galeri start
+// const closeButtons = document.querySelectorAll("#close-icon");
+// closeButtons.forEach((close) => {
+//   close.onclick = (e) => {
+//     detailModal1.style.display = "none";
+//     detailModal2.style.display = "none";
+//     e.preventDefault();
+//   }
+// })
+
+
+const detailModal1 = document.querySelector('#item-detail1');
+const button1 = document.querySelector('.cta-detail1');
+button1.onclick = (e) => {
+  detailModal1.style.display = "flex";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === detailModal1){
+    detailModal1.style.display = "none";
+  }
+};
+
+document.querySelector("#close-icon1").onclick = (e) => {
+  detailModal1.style.display = "none";
+  e.preventDefault();
+};
+
+
+const detailModal2 = document.querySelector('#item-detail2');
+const button2 = document.querySelector('.cta-detail2');
+button2.onclick = (e) => {
+  detailModal2.style.display = "flex";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === detailModal2){
+    detailModal2.style.display = "none";
+  }
+};
+
+document.querySelector("#close-icon2").onclick = (e) => {
+  detailModal2.style.display = "none";
+  e.preventDefault();
+};
+// galeri end
+
+
